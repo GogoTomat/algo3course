25728065fa73fec7a93bd43ecd62a6f32784c5d4
Mark Komissarov <mkomissarov207@gmail.com>
2022-09-11 14:10:13 +0500

    first task fix

--- 1lab/main.py
+++ 1lab/main.py
@@ -17,7 +17,8 @@ def randN(left, right, n):
 
 def revRandN(left, right, n):
     for i in range(0, n):
-        a.insert(0, int((right - left) * i / n + left))
+        a.append(int((right - left) * i / n + left))
+    a.reverse()
 
 
 def interSaw(left, right, n, k):
@@ -32,24 +33,25 @@ def interSin(left, right, n, k):
         a.append(int((right - left) * abs(math.cos((i % (interval + 1)) / interval * math.pi)) + left))
 
 
+def quaz(left, right, n):
+    diff = (right - left) // n
+    for i in range(n):
+        a.append(i * diff + left + random.randint(0, diff * 2))
+
+
 def step(left, right, n, k):  # values between steps can be more or less than i step, but not more than i + 1 step.
-    # let's say that every step divided by 5
-    interval = math.ceil(n / k)
+    interval = n / k
+    delta = (right - left) // k
+    for i in range(0, k):
+        for j in range(0, int(interval)):
+            a.append(delta * i + random.randint(0, delta))
 
-
-# def quazStep(left, right, n, k):
-#     interval = math.ceil(n / k)
-#     for i in range(0, n):
-#         if i % 5 != 0:
-#             a.append(random.randint(i // 5, (i // 5) + 5))
-#         else:
-#             a.append(i / 5)
 
 if __name__ == "__main__":
-    # for n in range(500000, 5000001, 500000):
-    #     start = time.time()
-    #     interSin(1, 10000000, n, 50000)
-    #     stop = time.time()
-    #     print(f'{n} - {stop - start}')
-    #     a = []
-    print(a)
+    for n in range(500000, 5000001, 500000):
+        start = time.time()
+        quaz(1, 10000000, n)
+        stop = time.time()
+        print(f'{stop - start:2f}')
+        a = []
+        rand(1, 100000, n)
